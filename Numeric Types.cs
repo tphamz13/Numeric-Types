7ea34a16aa979e2e2975b23123856880c0f1eb04
tphamz13 <tienpham_vu@yahoo.com>
2023-03-11 14:33:38 -0600

    first commit

---
+++ Numeric Types.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NumericTypes
+{
+    class Program
+     {
+        public static void Main()
+        {
+            Console.WriteLine("Enter length of the rectangle: ");
+            int Length = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter width of the rectangle: ");
+            int Width = Convert.ToInt32(Console.ReadLine());
+
+            int Area = Length * Width;
+            Console.WriteLine("The area of the rectable = " + Area);
+
+        }
+     }
+
+}
